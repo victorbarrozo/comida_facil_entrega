b8a31bd8d9bd8decfb1d0890085e19bcf3932156
victorbarrozo <victorbarrozoads@gmail.com>
2024-04-21 20:22:58 -0300

    navegação principal finalizada e funcional

--- app/src/main/java/com/victorbarrozo/comidafacil/MainActivity.kt
+++ app/src/main/java/com/victorbarrozo/comidafacil/MainActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>( R.id.btm_nav )
+        val navController = Navigation.findNavController( this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController( bottomNavigationView, navController )
     }
 }
